2785f78842792b06a393b9863fa746a089f97ba5
Avelino Mendes <avelinopopping@gmail.com>
2017-10-23 17:45:22 -0300

    commit initial

---
+++ features/step_definitions/game_steps.rb
@@ -0,0 +1,8 @@
+
+Quando /^começo um novo jogo$/ do
+	game = Game.new
+	game.start
+end
+Então /^vejo na tela:$/ do |string|
+	pending
+end
---
+++ spec/game_spec.rb
@@ -0,0 +1,15 @@
+require 'spec_helper'
+require 'game'
+
+describe Game do
+  describe "#start" do
+    it "prints the initial message" do
+      output = double("output")
+      game = Game.new(output)
+      
+      initial_message = "Bem vindo ao jogo da forca!"
+      output.to receive(:puts).with(initial_message)
+      game.start
+    end
+  end
+end
