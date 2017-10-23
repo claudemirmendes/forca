ed15b5923d88ae08e1c297407a1e2e61161c2900
Avelino Mendes <avelinopopping@gmail.com>
2017-10-23 17:51:09 -0300

    comented erro to-do

--- lib/game.rb
+++ lib/game.rb
@@ -3,6 +3,9 @@ class Game
 	def initialize(output = STDOUT)
 		@output = output
 	end
+
+	# TO-DO
+	# #<Double "output"> received unexpected message :to with
 	def start
 		initial_message = "Bem vindo ao jogo da forca!"
 		@output.puts initial_message
